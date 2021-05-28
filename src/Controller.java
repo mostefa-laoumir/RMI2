44f2fe9a428add254d537cf562a284154023af1e
mostefa <21nounou21@gmail.com>
2021-05-29 00:33:24 +0100

    final commit

--- src/Controller.java
+++ src/Controller.java
@@ -4,7 +4,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.InetAddress;
 import java.net.URL;
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
@@ -23,13 +25,14 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 verser.setOnAction(event -> {
     try {
-        Registry registry = LocateRegistry.getRegistry("192.168.43.239", 1099);
-        IRemote objet = (IRemote) registry.lookup("objet-calculatrice");
+
+        String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + ":9024/IRemote";
+        IRemote obj = (IRemote) Naming.lookup(url);
 
 
         String s = text.getText();
-        log.setText("Resultat : "+objet.Calculatrice(s));
-        System.out.println(objet.Calculatrice(s));
+        log.setText("Resultat : "+obj.Calculatrice(s));
+        System.out.println(obj.Calculatrice(s));
     } catch (Exception e) {
         e.printStackTrace();
     }
--- src/Serveur.java
+++ src/Serveur.java
@@ -1,4 +1,7 @@
+import java.net.InetAddress;
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Serveur {
@@ -6,8 +9,12 @@ public class Serveur {
     public static void main(String[] args) {
         try {
             RemoteImpl remoteImpl = new RemoteImpl();
-            IRemote objet = (IRemote) UnicastRemoteObject.exportObject(remoteImpl, 22222);
-            LocateRegistry.createRegistry(22222).bind("objet-calculatrice", objet);
+            IRemote objet = (IRemote) UnicastRemoteObject.exportObject(remoteImpl, 0);
+            Registry registry = LocateRegistry.createRegistry(9024);
+            System.out.println(InetAddress.getLocalHost().getHostAddress() );
+            String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + ":9024/IRemote";
+            Naming.rebind(url, remoteImpl);
+
             while (1==1) {
             }
         } catch (Exception e) {
