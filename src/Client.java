31b9da20ae938ba4d9d715bbac50d26e7d3c8afe
howkmii <abdelhakim@univ-tiaret.dz>
2021-09-30 20:46:55 +0100

    hacktoberfest21

--- src/Client.java
+++ src/Client.java
@@ -1,18 +1,19 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
 
-public class Client extends Application {
+public class Client {
 
-        @Override
-        public void start(Stage primaryStage) throws Exception{
-            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-            primaryStage.setTitle("Application Repartie");
-            primaryStage.setScene(new Scene(root, 500, 500));
-            primaryStage.show();
-        }
     public static void main(String[] args) {
-            launch(args);  }
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 10000);
+            IRemote objDis = (IRemote) registry.lookup("objDis");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("EQUATION :" );
+            String equation = scanner.next();
+            System.out.println("Resultat : "+objDis.calcule(equation));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
